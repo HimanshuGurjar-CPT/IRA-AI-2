f7a4b098bcfa35cb0b9e0a52072d5ec177cb72bf
Harsh <pandeygautam039@gmail.com>
2026-05-03 21:23:41 +0530

    feat: implement login screen with navigation to home route

--- app/login.tsx
+++ app/login.tsx
@@ -4,11 +4,11 @@ import { Pressable, Text, View } from "react-native";
 
 const login = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-400">
+    <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">Login</Text>
 
       <Link href="/" asChild>
-        <Pressable className="px-8 py-4 bg-primary rounded-xl mt-4">
+        <Pressable className="px-8 py-4 bg-red-500 rounded-xl mt-4">
           <Text className="text-black">Back to Home</Text>
         </Pressable>
       </Link>
