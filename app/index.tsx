c9313ee93fd4074dc6f52949bbd4d1debeb4b17b
Harsh <pandeygautam039@gmail.com>
2026-05-03 21:24:01 +0530

    feat: create landing page with login navigation using Nativewind styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View, Pressable } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -8,10 +8,9 @@ export default function App() {
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
-      
-      {/* Use asChild and style the Pressable instead */}
+
       <Link href="/login" asChild>
-        <Pressable className="p-4 bg-primary rounded-xl mt-4">
+        <Pressable className="p-4 bg-red-500 rounded mt-4">
           <Text className="text-black">Login</Text>
         </Pressable>
       </Link>
--- app/login.tsx
+++ app/login.tsx
@@ -8,7 +8,7 @@ const login = () => {
       <Text className="text-xl font-bold text-blue-500">Login</Text>
 
       <Link href="/" asChild>
-        <Pressable className="px-8 py-4 bg-red-500 rounded-xl mt-4">
+        <Pressable className="px-8 py-4 bg-red-500 rounded mt-4">
           <Text className="text-black">Back to Home</Text>
         </Pressable>
       </Link>
