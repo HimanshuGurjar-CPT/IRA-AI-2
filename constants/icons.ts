13385bba109c1f162765c9e48db1522d84fe27e5
Harsh <pandeygautam039@gmail.com>
2026-05-05 18:20:37 +0530

    feat: add constant object for application icons mapping

---
+++ constants/icons.ts
@@ -0,0 +1,11 @@
+import apps from "../assets/icons/apps.png";
+import home from "../assets/icons/home.png";
+import notes from "../assets/icons/notes.png";
+import profile from "../assets/icons/profile.png";
+
+export default {
+  apps,
+  home,
+  notes,
+  profile,
+};
