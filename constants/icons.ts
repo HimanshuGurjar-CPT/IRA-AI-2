d4aedd1972260e9b782ddda9aada72a6d460da9f
Harsh <pandeygautam039@gmail.com>
2026-05-05 19:31:57 +0530

    feat: add icon constants and IRIS-X SVG asset

--- constants/icons.ts
+++ constants/icons.ts
@@ -2,10 +2,12 @@ import apps from "../assets/icons/apps.png";
 import home from "../assets/icons/home.png";
 import notes from "../assets/icons/notes.png";
 import profile from "../assets/icons/profile.png";
+import logo from "../assets/images/iris-x.svg";
 
 export default {
   apps,
   home,
   notes,
   profile,
+  logo,
 };
