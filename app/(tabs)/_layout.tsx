c8a1ceff86131cf60857949fcf16241e7cb90802
Harsh <pandeygautam039@gmail.com>
2026-05-05 18:37:29 +0530

    feat: implement dynamic tab bar layout using tab configuration data

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,17 @@
 import { tabs } from "@/constants/data";
 import { Image } from "expo-image";
 import { Tabs } from "expo-router";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+
+const TabIcon = ({ focused, icon }: TabIconProps) => {
+  return (
+    <View className="tabs-icon">
+      <View className={clsx("tabs-pill", focused && "tabs-active")}>
+        <Image source={icon} resizeMode="contain" className="tabs-glyph" />
+      </View>
+    </View>
+  );
+};
 
 const TabLayout = () => (
   <Tabs screenOptions={{ headerShown: false }}>
