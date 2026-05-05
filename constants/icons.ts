86f832a12346f02710320b4a0fddef8fa70dc0f9
Harsh <pandeygautam039@gmail.com>
2026-05-05 19:39:45 +0530

    feat: add icons constant file and iris_x_icon asset

--- constants/icons.ts
+++ constants/icons.ts
@@ -2,7 +2,7 @@ import apps from "../assets/icons/apps.png";
 import home from "../assets/icons/home.png";
 import notes from "../assets/icons/notes.png";
 import profile from "../assets/icons/profile.png";
-import logo from "../assets/images/iris-x.svg";
+import logo from "../assets/images/iris_x_icon.png";
 
 export default {
   apps,
