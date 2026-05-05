6df7f29712e61fc48ce64717ab0598542b0fa57f
Harsh <pandeygautam039@gmail.com>
2026-05-05 18:28:00 +0530

    feat: add tab navigation configuration constants

--- constants/data.ts
+++ constants/data.ts
@@ -1,6 +1,8 @@
+import icons from "./icons";
+
 export const tabs: AppTab[] = [
   { name: "index", title: "Home", icon: icons.home },
-  { name: "subscriptions", title: "Subscriptions", icon: icons.wallet },
-  { name: "insights", title: "Insights", icon: icons.activity },
-  { name: "settings", title: "Settings", icon: icons.setting },
+  { name: "apps", title: "Apps", icon: icons.apps },
+  { name: "notes", title: "Notes", icon: icons.notes },
+  { name: "settings", title: "Settings", icon: icons.profile },
 ];
