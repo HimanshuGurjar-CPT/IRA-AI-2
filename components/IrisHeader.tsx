062f227508ff862ccd928d1bfabff6e0e8e85ce4
Harsh <pandeygautam039@gmail.com>
2026-05-05 19:44:30 +0530

    feat: add IrisHeader component with application logo and centralized icon assets

--- components/IrisHeader.tsx
+++ components/IrisHeader.tsx
@@ -5,9 +5,9 @@ import { Text, View } from "react-native";
 
 const IrisHeader = () => {
   return (
-    <View>
-      <Image source={icons.logo} className="w-12 h-12" />
-      <Text className="text-green-400 text-xl">IRIS</Text>
+    <View className="flex flex-row items-center gap-2">
+      <Image source={icons.logo} className="w-20 h-20" />
+      <Text className="text-white text-4xl font-bold text-center">IRIS</Text>
     </View>
   );
 };
--- constants/icons.ts
+++ constants/icons.ts
@@ -1,8 +1,8 @@
 import apps from "../assets/icons/apps.png";
 import home from "../assets/icons/home.png";
+import logo from "../assets/icons/logo.png";
 import notes from "../assets/icons/notes.png";
 import profile from "../assets/icons/profile.png";
-import logo from "../assets/images/iris_x_icon.png";
 
 export default {
   apps,
