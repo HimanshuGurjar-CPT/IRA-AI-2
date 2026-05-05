0076489f1899a36e8c27f289f598fd5afbdb3254
Harsh <pandeygautam039@gmail.com>
2026-05-05 19:05:50 +0530

    feat: implement landing page for IRIS-X tabs interface

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,16 @@
-import { Text } from "react-native";
 import { styled } from "nativewind";
+import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
-const SafeAreaView = styled(RNSafeAreaView)
+const SafeAreaView = styled(RNSafeAreaView);
 
 const index = () => {
   return (
-    <SafeAreaView className="flex-1 items-center">
-      <Text>index</Text>
+    <SafeAreaView className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white text-6xl">IRIS-X</Text>
+      <Text className="text-white text-lg mt-4 text-center">
+        Empowering visually impaired with AI
+      </Text>
     </SafeAreaView>
   );
 };
