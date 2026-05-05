f540f8131dfd1fcbf73bf5d569753e73b2fac4f6
Harsh <pandeygautam039@gmail.com>
2026-05-05 19:19:39 +0530

    feat: implement custom bottom navigation layout with dynamic tab rendering

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -49,7 +49,7 @@ const TabLayout = () => {
           height: tabBar.height,
           marginHorizontal: tabBar.horizontalInset,
           borderRadius: tabBar.radius,
-          backgroundColor: "#000",
+          backgroundColor: "#1C1C1C",
           borderTopWidth: 0,
           elevation: 0,
         },
