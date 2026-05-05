215f6ff9043ff761ee3397201553bf06e43f17e3
Harsh <pandeygautam039@gmail.com>
2026-05-05 21:16:53 +0530

    feat: add IrisHeader component and logo asset

--- components/IrisHeader.tsx
+++ components/IrisHeader.tsx
@@ -1,14 +1,8 @@
-import icons from "@/constants/icons";
-import { Image } from "expo-image";
 import React from "react";
 import { View } from "react-native";
 
 const IrisHeader = () => {
-  return (
-    <View className="flex flex-row items-center gap-2">
-      <Image source={icons.logo} style={{ width: 100, height: 80 }} />
-    </View>
-  );
+  return <View className="flex flex-row items-center gap-2"></View>;
 };
 
 export default IrisHeader;
