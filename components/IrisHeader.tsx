9e757168d74845181c14d489db4ee414d0482a08
Harsh <pandeygautam039@gmail.com>
2026-05-05 19:32:43 +0530

    feat: add IrisHeader component with logo and text branding

--- components/IrisHeader.tsx
+++ components/IrisHeader.tsx
@@ -1,3 +1,4 @@
+import icons from "@/constants/icons";
 import { Image } from "expo-image";
 import React from "react";
 import { Text, View } from "react-native";
@@ -5,7 +6,7 @@ import { Text, View } from "react-native";
 const IrisHeader = () => {
   return (
     <View>
-      <Image source={require("@/assets/logo.png")} className="w-12 h-12" />
+      <Image source={icons.logo} className="w-12 h-12" />
       <Text className="text-green-400 text-xl">IRIS</Text>
     </View>
   );
