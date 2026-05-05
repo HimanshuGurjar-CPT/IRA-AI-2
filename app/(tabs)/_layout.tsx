d43fa3111d3b841aa6528f4c27047cd00e203f85
Harsh <pandeygautam039@gmail.com>
2026-05-05 18:29:05 +0530

    feat: implement dynamic tab bar layout using tab configuration data

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,23 @@
+import { tabs } from "@/constants/data";
+import { Image } from "expo-image";
 import { Tabs } from "expo-router";
 
 const TabLayout = () => (
   <Tabs>
-    <Tabs.Screen name="index" options={{ title: "Home" }} />
-    <Tabs.Screen name="apps" options={{ title: "Apps" }} />
-    <Tabs.Screen name="notes" options={{ title: "Notes" }} />
-    <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+    {tabs.map((tab) => {
+      return (
+        <Tabs.Screen
+          key={tab.name}
+          name={tab.name}
+          options={{
+            title: tab.title,
+            tabBarIcon: () => (
+              <Image source={tab.icon} style={{ width: 24, height: 24 }} />
+            ),
+          }}
+        />
+      );
+    })}
   </Tabs>
 );
 
