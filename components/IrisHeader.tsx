608782d2173f25581c9e64df735a307ece859993
Harsh <pandeygautam039@gmail.com>
2026-05-06 20:48:42 +0530

    feat: add IrisHeader component with branding and status icon

--- components/IrisHeader.tsx
+++ components/IrisHeader.tsx
@@ -1,11 +1,20 @@
+import { SignalHighIcon } from "lucide-react-native";
 import React from "react";
 import { Text, View } from "react-native";
 
 const IrisHeader = () => {
   return (
     <>
+      <View className="h-24 bg-green-400 w-full flex-row">
+        <SignalHighIcon color="white" size={28} />
+      </View>
       <View>
-        <Text className="text-white text-6xl">IRIS-X</Text>
+        <Text className="text-white text-xl mt-4 text-center font-outfit-medium">
+          IRIS-X
+        </Text>
+        <Text className="text-white text-xl mt-4 text-center font-outfit-medium">
+          Artificial Intelligence
+        </Text>
       </View>
     </>
   );
