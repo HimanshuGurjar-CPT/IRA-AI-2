acc90c6749be02bb7930dd99c6551b4475761074
Harsh <pandeygautam039@gmail.com>
2026-05-07 20:38:41 +0530

    feat: add IrisHeader component with logo, signal icon, and notification bell

--- components/IrisHeader.tsx
+++ components/IrisHeader.tsx
@@ -10,7 +10,7 @@ const IrisHeader = () => {
         <View>
           <Text className="text-white text-3xl text-center font-antonio-bold tracking-wider">
             IRIS
-            <Text className="text-text-main">-X</Text>
+            <Text className="text-main">-X</Text>
           </Text>
         </View>
         <View>
