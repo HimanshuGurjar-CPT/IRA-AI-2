cbf582792153dc9a08bb5b668c4b2e8fa19874a2
Harsh <pandeygautam039@gmail.com>
2026-05-07 20:55:55 +0530

    feat: create CommandCard component for command interface

---
+++ components/CommandCard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CommandCard = () => {
+  return (
+    <View>
+      <Text>CommandCard</Text>
+    </View>
+  )
+}
+
+export default CommandCard
